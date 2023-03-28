5266b11538359821def5d74a10e0430e7f1f18fd
George Smyridis <70004539+georgesmyr@users.noreply.github.com>
2023-03-28 20:23:43 +0300

    SudokuSolver Update

--- SASudokuSolver/SASudokuSolver/main.cpp
+++ SASudokuSolver/SASudokuSolver/main.cpp
@@ -7,6 +7,7 @@
 #include <vector>
 #include <algorithm>
 #include <unordered_set>
+#include <random>
 
 #include "mt19937.h"
 
@@ -20,7 +21,7 @@ class SudokuSolver{
 private:
     
     double BETA {1};
-    const double COOLING_RATE {0.000001};
+    const double COOLING_RATE {0.99};
     int board[BOARD_SIZE][BOARD_SIZE] = {
         {0, 0, 9, 0, 7, 0, 0, 0, 2},
         {0, 0, 0, 0, 0, 8, 4, 0, 0},
@@ -32,7 +33,24 @@ private:
         {0, 0, 0, 3, 0, 0, 0, 0, 0},
         {6, 0, 0, 0, 8, 0, 0, 0, 0}
     };
-        
+    
+    unordered_set<int> GetAllPossibleValues(){
+        /*
+         Description:
+         ------------
+         For given size of sudoku instance, it returns an unordered set
+         with all the possible values that one can place in the cells.
+         */
+        unordered_set<int> values = {};
+        for (int i = 1; i < BOARD_SIZE + 1; ++i){
+            assert(i >= 1 && i <= BOARD_SIZE);
+            values.emplace(i);
+        }
+        return values;
+    }
+    unordered_set<int> AllPossibleValues = GetAllPossibleValues();
+    
+    
     vector<vector<int>> GetFixedPositions(){
         /*
          Description:
@@ -41,28 +59,27 @@ private:
          These elements cannot be altered during the solution of the game.
          */
         
-        vector<vector<int>> constPos = {};
-        for (auto row = 0; row < N; ++row){
-            for (auto col = 0; col < N; ++col){
+        vector<vector<int>> FixedPos = {};
+        for (auto row = 0; row < BOARD_SIZE; ++row){
+            for (auto col = 0; col < BOARD_SIZE; ++col){
                 if (board[row][col] != 0){
-                    constPos.push_back({row,col});
+                    FixedPos.push_back({row,col});
                 }
             }
         }
-        return constPos;
+        return FixedPos;
     }
-    
     const vector<vector<int>> FixedPositions = GetFixedPositions();
-    int costFunction = 0;
-    vector<int> randomPos = {};
-    int randomEntry = 0;
-    bool solved = false;
+
     
 public:
     
-    void preFillBoard(){}
-    
     void print_board() {
+        /*
+         Description:
+         ------------
+         Prints the grid with the current values of each cell.
+         */
         for (int i = 0; i < BOARD_SIZE; i++) {
             if (i % N == 0 && i != 0) {
                 cout << "---------------------" << endl;
@@ -77,29 +94,139 @@ public:
         }
     }
     
-    void InitialiseBoard(){
-        
+    void PreFill(){}
+    
+    vector<int> GetSquare(vector<int> cell){
+        /*
+         Description:
+         ------------
+         It returns the box that the given cell belongs to.
+         */
+        int i = cell[0] / N;
+        int j = cell[1] / N;
+        assert(i >=0 && i <= N);
+        assert(j >=0 && j <= N);
+        return {i, j};
     }
     
+    int GetRandomElement(unordered_set<int> set){
+        /*
+         Description:
+         ------------
+         Given an unordered set as input, it selects and returns one of its
+         elements randomly.
+         */
+        if (set.size() == 1){
+            for(auto it = set.begin(); it != set.end();){
+                return *it;
+            }
+        }
+        random_device rd;
+        mt19937 gen(rd());
+        uniform_int_distribution<> dis(0, set.size() - 1);
+        auto it = set.begin();
+        advance(it, dis(gen));
+        int randomElement = *it;
+        return randomElement;
+    }
+
+    void InitialiseSolution(){
+        /*
+         Description:
+         ------------
+         It generates a random solution. It places random numbers in the non-fixed cells of each
+         box, so that all the numbers are contained exactly once in each box.
+         */
+
+        int rowLow = 0, rowHigh = 0;
+        int colLow = 0, colHigh = 0;
+
+        // Iteration over all squares
+        for (int i = 0; i < N; ++i){
+            rowLow = i * N;
+            rowHigh = (i + 1) * N - 1;
+            for (int j = 0; j < N; ++j){
+                colLow = j * N;
+                colHigh = (j + 1) * N - 1;
+                // Iteration over all cells inside the square
+                // Find all the allowed values we can assign in the box.
+                unordered_set<int> AllowedValues = AllPossibleValues;
+                for (int row = rowLow; row < rowHigh + 1; ++row){
+                    for (int col = colLow; col < colHigh + 1; ++col){
+                        AllowedValues.erase(board[row][col]);
+                    }
+                }
+
+                // Iteration over all cells inside the square.
+                // Put random values in the non-fixed cells
+                // so that all of them are unique.
+                for (int row = rowLow; row < rowHigh + 1; ++row){
+                    for (int col = colLow; col < colHigh + 1; ++col){
+                        //cout << row << " - " << col << endl;
+                        // If the cell is fixed, go to the next.
+                        vector<int> pos = {row, col};
+                        if (find(FixedPositions.begin(), FixedPositions.end(), pos) != FixedPositions.end()){
+                            //cout << "Cannot change entry of pre-filled cell (" << row << "," << col << "). It remains: " << board[row][col] << endl;
+                            continue;
+                        }
+
+                        // Otherwise, randomly assign a number to it so that it's unique in the box.
+                        if (AllowedValues.size() > 0){
+                            int randomInteger = GetRandomElement(AllowedValues);
+                            AllowedValues.erase(randomInteger);
+                            board[row][col] = randomInteger;
+                            //cout << "Setting cell (" << row << "," << col << ") = " << randomInteger << endl;
+                        }
+                    }
+                }
+            }
+        }
+        print_board();
+    }
     
+    vector<int> GenRanPosInGrid(){
+        /*
+         Description:
+         -----------
+         Generates random position from all elements
+         */
+        
+        // Pick a random position, not one of the initial ones, and put a random integer in it.
+        vector<int> randomPos = {};
+        do {
+            randomPos = {(int) round(dsfmt_genrand() * BOARD_SIZE), (int) round(dsfmt_genrand() * BOARD_SIZE)};
+        } while(find(FixedPositions.begin(), FixedPositions.end(), randomPos) != FixedPositions.end());
+        
+        assert (randomPos[0] >= 0 and randomPos[0] <= 8);
+        assert (randomPos[1] >= 0 and randomPos[1] <= 8);
+        
+        return randomPos;
+    }
     
-//    void GenRandomPosition(){
-//
-//        // Pick a random position, not one of the initial ones, and put a random integer in it.
-//        do {
-//            randomPos = {int (dsfmt_genrand() * N), int (dsfmt_genrand() * N)};
-//        } while(find(constPos.begin(), constPos.end(), randomPos) != constPos.end());
-//
-//        assert (randomPos[0] >= 0 and randomPos[0] <= 8);
-//        assert (randomPos[1] >= 0 and randomPos[1] <= 8);
-//    }
-//
-//    void GenRandomEntry(){
-//
-//        randomEntry = int (dsfmt_genrand() * N + 1);
-//        assert (randomEntry >= 1 and randomEntry <= 9);
-//    }
-//
+    vector<int> GenRanPosInSquare(vector<int> square){
+        
+        int rowLow = square[0] * N, rowHigh = (square[0] + 1) * N - 1;
+        int colLow = square[1] * N, colHigh = (square[1] + 1) * N - 1;
+        
+        int row = rowLow + round(dsfmt_genrand() * (rowHigh - rowLow));
+        int col = colLow + round(dsfmt_genrand() * (colHigh - colLow));
+        vector<int> pos = {row, col};
+        
+        return pos;
+    }
+
+    int GenRandomEntry(){
+        int randomEntry = 0;
+        /*
+         Description:
+         ------------
+         Generates random entry, i.e. a number from 1 to BOARD_SIZE.
+         */
+        randomEntry = 1 + round(dsfmt_genrand() * (BOARD_SIZE - 1));
+        assert (randomEntry >= 1 and randomEntry <= BOARD_SIZE);
+        return randomEntry;
+    }
+
 //    int getCostFunction(){
 //
 //        int duplicates  = 0;
@@ -220,12 +347,17 @@ int main(int argc, const char * argv[]) {
     dsfmt_seed( time (NULL)); // Initialise random seed.
 
     SudokuSolver sudoku;
-    sudoku.print_board();
+    //sudoku.print_board();
+    //sudoku.InitialiseSolution();
+    //sudoku.GenRandomPosition();
     
-    sudoku.solve();
-
-    
-    
+    unordered_set<int> vals = {1,2,3,4,5,6,7,8,9};
+    int entry = 0;
+    do{
+        entry = sudoku.GenRandomEntry();
+        cout << entry << endl;
+        vals.erase(entry);
+    }while(vals.size() > 0);
     
     return 0;
 }
